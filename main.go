ecea9baa04b2ad16568a18b77113c4d9e5010287
crossedbot <59076483+crossedbot@users.noreply.github.com>
2020-02-01 11:23:01 -0500

    moved db and logger to common repo

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"os"
 	"strings"
 	"time"
 
+	"github.com/jinzhu/gorm"
+
+	database "github.com/crossedbot/common/golang/db"
+	"github.com/crossedbot/common/golang/logger"
 	"github.com/crossedbot/warpath/config"
-	database "github.com/crossedbot/warpath/db"
-	"github.com/crossedbot/warpath/logger"
 	"github.com/crossedbot/warpath/warpath"
 )
 
@@ -27,24 +29,25 @@ type Flags struct {
 func main() {
 	f := flags()
 	c := configuration()
-	db, err := database.New(&c)
+	logger.SetFile(c.Logging.File)
+	db, err := newDB(&c)
 	if err != nil {
-		log.Fatal(err)
+		fatal("failed to create database: %s", err.Error())
 	}
 	defer db.Close()
 	warpath_t, err := warpath.New(f.Device, c.SnapLength, (time.Duration(c.Timeout) * time.Second))
 	if err != nil {
-		log.Fatal(err)
+		fatal("failed to create Warpath handler: %s", err.Error())
 	}
 	go func() {
 		if err := warpath_t.Start(filter(c.Filter)); err != nil {
-			log.Fatal(err)
+			fatal("failed to start capture: %s", err.Error())
 		}
 	}()
 	loop := f.Count == -1
 	for frame := range warpath_t.Output() {
 		if err := frame.Save(db); err != nil {
-			logger.Log.Error(err)
+			logger.Error(err)
 		}
 		if !loop {
 			if f.Count <= 1 {
@@ -54,6 +57,11 @@ func main() {
 		}
 	}
 	warpath_t.Stop()
+}
+
+func fatal(format string, a ...interface{}) {
+	logger.Error(fmt.Errorf(format, a...))
+	os.Exit(1)
 }
 
 func flags() Flags {
@@ -70,6 +78,30 @@ func configuration() config.Config {
 	var c config.Config
 	config.Load(&c)
 	return c
+}
+
+func newDB(c *config.Config) (*gorm.DB, error) {
+	db, err := database.New(
+		c.Database.Name,
+		c.Database.Path,
+		c.Database.MaxOpenConnections,
+	)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(c.Logging.Mode)
+	db.SetLogger(logger.Log)
+	if err := database.Migrate(
+		db.DB(),
+		c.Database.Name,
+		c.Database.Path,
+		c.Database.MigrationsPath,
+		c.Database.MigrationsEnv,
+	); err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
 
 func filter(filterBy string) (filter string) {
